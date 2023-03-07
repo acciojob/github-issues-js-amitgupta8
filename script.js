5229d04fb8db926f0efc2f1a2ba8996b03baa859
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-08 00:00:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+$(document).ready(function(){
+			var pageNumber = 1;
+
+			function loadIssues(pageNumber){
+				$.ajax({
+					url: `https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`,
+					method: 'GET',
+					success: function(response){
+						$('#issues_list').empty(); //clear the list before adding new issues
+						$.each(response, function(index, issue){
+							var issueName = issue.title;
+							$('#issues_list').append(`<li>${issueName}</li>`);
+						});
+						$('h1').text(`Page number ${pageNumber}`);
+					}
+				});
+			}
+
+			loadIssues(pageNumber); //load the issues for page 1 by default
+
+			$('#load_prev').click(function(){
+				if(pageNumber > 1){ //don't go back if on page 1
+					pageNumber--;
+					loadIssues(pageNumber);
+				}
+			});
+
+			$('#load_next').click(function(){
+				pageNumber++;
+				loadIssues(pageNumber);
+			});
+		});
